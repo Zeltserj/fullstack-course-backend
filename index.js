47001fde6f0ca9339e7425295c83e187ad19bfd4
Jonathan Zeltser <zeltserj@gmail.com>
2024-10-02 09:57:43 +0300

    Deploying froented and backend using fly.io

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
 morgan.token("post-data", (req, res) => {
   if (req.method == "POST") {
     return JSON.stringify(req.body);
   }
   return "";
 });
+app.use(express.static("dist"));
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length]  :response-time ms :post-data "
   )
 );
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const uuid = () => {
   return Math.floor(Math.random() * 1000000).toString(16);
